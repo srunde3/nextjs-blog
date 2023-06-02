e1280616d08b1238529a1d783c6f33003245efd1
runde3wisc <47787784+runde3wisc@users.noreply.github.com>
2023-06-02 13:03:14 -0500

    add statically rendered blog posts

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,18 @@ import Head from "next/head";
 import Link from "next/link";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
 
-export default function Home() {
+export async function getStaticProps() { // function name is Next.js convention
+  const allPostsData = getSortedPostsData();
+  return { // returning a JSON structure
+    props: {  // props is a Next.js convention
+      allPostsData,
+    },
+  }; // semicolon here for the end of our JavaScript statement
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -12,6 +22,20 @@ export default function Home() {
       <section className={utilStyles.headingMd}>
         <p>Software engineer, guitarist, music-lover, dickhead coffee snob</p>
         <p><Link href="/posts/first-post">My first post!</Link></p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
