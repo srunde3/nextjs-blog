64ced1867fd97541cd0d220dddfd30591fd4f89c
runde3wisc <47787784+runde3wisc@users.noreply.github.com>
2023-06-03 08:27:40 -0500

    remove first post

--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,6 @@ import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
 
-export async function getStaticProps() { // function name is Next.js convention
-  const allPostsData = getSortedPostsData();
-  return { // returning a JSON structure
-    props: {  // props is a Next.js convention
-      allPostsData,
-    },
-  }; // semicolon here for the end of our JavaScript statement
-}
-
 export default function Home({ allPostsData }) {
   return (
     <Layout home>
@@ -21,7 +12,6 @@ export default function Home({ allPostsData }) {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Software engineer, guitarist, music-lover, dickhead coffee snob</p>
-        <p><Link href="/posts/first-post">My first post!</Link></p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
@@ -39,4 +29,13 @@ export default function Home({ allPostsData }) {
       </section>
     </Layout>
   );
+}
+
+export async function getStaticProps() { // function name is Next.js convention
+  const allPostsData = getSortedPostsData();
+  return { // returning a JSON structure
+    props: {  // props is a Next.js convention
+      allPostsData,
+    },
+  }; // semicolon here for the end of our JavaScript statement
 }
